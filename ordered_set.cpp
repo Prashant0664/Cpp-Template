32f7991eba2236d6a1b7804da991b01a22469e15
prashant0664 <prashantthakran0664@gmail.com>
2024-03-03 13:00:53 +0530

    added os

---
+++ ordered_set.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define ordered_set tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update>
+
+// Finding the kth smallest element
+// in the set using * because
+//  find_by_order returns an iterator
+// *(o_set.find_by_order(k))
+
+// Finding the number of elements
+// strictly less than k
+// s.order_of_key(k)
+
+int main()
+{
+    ordered_set s;
+    s.insert(1);
+    s.find_by_order(1);
+    return 0;
+}

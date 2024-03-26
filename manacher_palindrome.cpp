9a9fd645789ad6c02150c8b1f8550db813158c74
prashant0664 <prashantthakran0664@gmail.com>
2024-03-26 22:32:52 +0530

    palindrome

--- manacher_palindrome.cpp
+++ manacher_palindrome.cpp
@@ -14,3 +14,52 @@ vector<int> manacher_odd(string s) {
     }
     return vector<int>(begin(p) + 1, end(p) - 1);
 }
+
+string longestPalindrome(string s) {
+    int n = s.length();
+    vector<char> newStr(2*n+1);
+    int i = 0;
+    newStr[i++] = '#';
+    for(char c : s){
+        newStr[i++] = c;
+        newStr[i++] = '#';
+    }
+
+    vector<int> p(2*n+1);
+    int center = 0, right = 0;
+
+    int longestLength = 0, longestCenter = 0;
+
+    for(i = 0; i < newStr.size(); i++){
+
+        int mirror = 2*center - i;
+
+        if(right > i){
+            p[i] = min(p[mirror], right - i);
+        }
+
+        int a = i + (p[i] + 1);
+        int b = i - (p[i] + 1);
+
+        while(b >= 0 && a < newStr.size() && newStr[a] == newStr[b]){
+            b--;
+            a++;
+            p[i]++;
+        }
+
+        if(p[i] >= longestLength){
+            longestCenter = i;
+            longestLength = p[i];
+        }
+
+        if(i + p[i] > right){
+            center = i;
+            right = i + p[i];
+        }
+    }
+
+    string st(newStr.begin(), newStr.end());
+    st = st.substr(longestCenter - longestLength, 2*longestLength);
+    st.erase(remove(st.begin(), st.end(), '#'), st.end());
+    return st;
+}
